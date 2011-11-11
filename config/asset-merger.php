a21911eadacb82498b66667bc89df620cac6a251
Ivan Kerin <ikerin@gmail.com>
2011-11-11 13:18:49 +0200

    Merge on staging by default

--- config/asset-merger.php
+++ config/asset-merger.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 return array(
-	'merge' => Kohana::PRODUCTION,
+	'merge' => array(Kohana::PRODUCTION, Kohana::STAGING),
 	'folder' => "assets",
 	"load_paths" => array(
 		Assets::JAVASCRIPT => DOCROOT.'js'.DIRECTORY_SEPARATOR,
