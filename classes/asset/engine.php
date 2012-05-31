ac212b0c4861cfda9ea9cde9b06dbbbf049159be
Ivan Kerin <ivan@openbuildings.com>
2012-05-31 12:41:27 +0300

    fix documentations

--- classes/asset/engine.php
+++ classes/asset/engine.php
@@ -4,7 +4,7 @@
 *
 * @package    OpenBuildings/asset-merger
 * @author     Ivan Kerin
-* @copyright  (c) 2011 OpenBuildings Inc.
+* @copyright  (c) 2011-2012 OpenBuildings Inc.
 * @license    http://creativecommons.org/licenses/by-sa/3.0/legalcode
 */
 class Asset_Engine {
