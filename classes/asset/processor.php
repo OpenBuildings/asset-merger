cac98dfc8dcb697972a45537c76ca597840fb275
Pete Saia <petesaia@ysaa.(none)>
2013-03-06 01:39:46 +0000

    removed capitalized files i could not see on os x

--- classes/asset/processor.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-
-class Asset_Processor extends Kohana_Asset_Processor {}
--- classes/asset/processor/jsminplus.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-
-class Asset_Processor_Jsminplus extends Kohana_Asset_Processor_Jsminplus {}
